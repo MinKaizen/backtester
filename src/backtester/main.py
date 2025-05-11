a03c8ac939838e3a71ffcaf7da0e9f50a4d7db9b
minkaizen <martin.g.cao@gmail.com>
2025-05-12 09:11:03 +1000

    feat: update main.py

--- src/backtester/main.py
+++ src/backtester/main.py
@@ -1,19 +1,25 @@
-from pprint import pprint
-from datetime import datetime
 from decimal import Decimal
-from models.candle import Candle
+import pandas as pd
+from backtester.backtest_runner import BacktestRunner
+from models.player import Player
+from strategies.random_strategy import RandomStrategy
 
 def main():
-    print("Hello world!")
-    candle = Candle(
-        timestamp=datetime.utcnow(),
-        open=Decimal("101.5"),
-        high=Decimal("105.0"),
-        low=Decimal("100.0"),
-        close=Decimal("103.2"),
-        volume=Decimal("1500.75"),
+    df = pd.read_csv(
+        "mock_data.csv",
+        dtype={
+            "timestamp": str,
+            "open": float,
+            "high": float,
+            "low": float,
+            "close": float,
+            "volume": float,
+        },
     )
-    pprint(candle.model_dump())
+    strategy = RandomStrategy()
+    player = Player(strategy=strategy, bankroll=Decimal("1000.0"))
+    runner = BacktestRunner(df, [player])
+    runner.run()
 
 if __name__ == "__main__":
     main()
